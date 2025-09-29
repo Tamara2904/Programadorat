9eafe4a3b17367d77e62c3ba836f725dc4e36285
Tamara2904 <i202518199@cibertec.edu.pe>
2025-09-29 18:38:28 +0000

    resultados de tarea

---
+++ 01_app.py
@@ -0,0 +1,9 @@
+#Operaciones matematicas basica
+# suma 
+print(4+5)
+# resta
+print(90-45)
+# multiplicacion
+print(43*50)
+#división
+print(120/40)
---
+++ 02_app.py
@@ -0,0 +1,9 @@
+#ELABORA LAS SIGUIENTES OPERACIONES 
+#SUMAR 144 MÁS 105
+print(144+105)
+#obtener 3 al cuadrado
+print(3**2)
+#calcular e imprimpir 9 multiplicado por 2
+print(9*2)
+#imprimir el resultad de 120 entre 2 
+print(120/2)
