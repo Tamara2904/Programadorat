096d52ddba4d4bb86bc6e56b3d9f1f5c54226232
Tamara2904 <i202518199@cibertec.edu.pe>
2025-09-29 22:18:06 +0000

    ejemplo 1

---
+++ 08_app.py
@@ -0,0 +1,12 @@
+# Calcular la edad de una persona a partir de su año de nacimiento 
+
+# Definir una variable que reciba el número de año de nacimient de la persona
+year_birthday= input("Ingresa el año de tu nacimiento: ")
+
+#Operación de cálculo de edad: año_actual - año_nacimiento
+
+edad = 2025 - int(year_birthday)
+
+#Vizualiza la edad
+
+print(f"La edad de la persona es: {edad}")
