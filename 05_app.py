1fe071eec60c3116c0cb6b2df3be278bc7c86d04
Tamara2904 <i202518199@cibertec.edu.pe>
2025-09-29 20:16:22 +0000

    Ejercicio de notas

---
+++ 05_app.py
@@ -0,0 +1,33 @@
+# Obtener el promedio de un estudiante a partir de 3 notas
+
+#inicio: definir variables de las notas
+nota1=20
+nota2=15
+nota3=14
+print(nota1)
+print(nota2)
+print(nota3)
+
+#proceso: Definir el proceso de cálcul del promedio de notas 
+
+Promedio_notas=(nota1+nota2+nota3)/3
+print(Promedio_notas)
+
+
+
+#salida: Mostra el resultado del promedio 
+
+print(f"El promedio es: {Promedio_notas}")
+
+
+#Obtener el promedio de un estudiante
+
+#inicio
+nota_t1=16
+nota_t2=10
+nota_t3=13
+#T1(20%)+T2(35%)+EF(45%)
+
+print(nota_t1*.2+nota_t2*.35+nota_t3*.45)
+
+
