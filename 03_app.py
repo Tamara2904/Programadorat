f3abb7809a1c4a75e14b8f6578e2b770ee823830
Tamara2904 <i202518199@cibertec.edu.pe>
2025-09-29 18:47:32 +0000

    vizualizando variables

---
+++ 03_app.py
@@ -0,0 +1,15 @@
+# Variables 
+#Las variables son palabras específicas que utilizamos para asignar y almacenar valores específicos que podemos recuperar en cualquier momento.
+
+
+# Variable nombre 
+nombre ="Tamara"
+telefono=926432538
+name="tamara"
+#Visualizando el contenido de las variables
+
+print(nombre)
+print(telefono)
+print(name)
+
+
