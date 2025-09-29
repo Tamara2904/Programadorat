dfd06ae81eac17a69c255512764bf15e1c45a3f8
Tamara2904 <i202518199@cibertec.edu.pe>
2025-09-29 19:53:10 +0000

    ejercicios con igv

---
+++ 04_app.py
@@ -0,0 +1,15 @@
+# Calcular el 18% de IGV a aprtir de un monto de pago
+# Inicio: Definir o crear variables
+
+monto_pago = 600
+igv = 0.18
+
+# Proceso: Realizar las operaciones de cálculo
+monto_a_pagar = monto_pago + (monto_pago + igv)
+
+# Salida: Visualización de resultados
+print(monto_a_pagar)
+
+print(f"el monto es: {monto_pago}")
+print(f"el igc es: {igv}")
+print(f"el monto a pagar:{monto_a_pagar}")
