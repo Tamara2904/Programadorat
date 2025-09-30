3d11ee2b8d66fa6b68fd37a51aafc314b285d7ad
Tamara2904 <i202518199@cibertec.edu.pe>
2025-09-30 01:58:29 +0000

    ejercicios de else

--- 15_app.py
+++ 15_app.py
@@ -1,10 +1,10 @@
-comprobante = "factura"
-monto= 100
-if comprobante = "factura"
-  mont_igv = monto * 0.18
+comprobante = "boleta"
+monto= 5000
+if comprobante =="factura":
+  monto_igv= monto * 0.18
   monto_a_pagar = monto + monto_igv
   print(f"El monto total a pagar con factura es: {monto_a_pagar}")
-elif comprobante = "boleta"
+elif comprobante =="boleta":
     print(f"El monto total a pagar con boleta es: {monto}")
 
 else:
